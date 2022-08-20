0a8a03361a42048276edbd28c7f60f81afe364c1
loures90 <fernando.loures@hotmail.com>
2022-08-20 10:23:22 -0300

    test: ensure AAddProductService throws when ProductRepository Throws

--- infinity-back/src/test/services/add-product.service.spec.ts
+++ infinity-back/src/test/services/add-product.service.spec.ts
@@ -22,6 +22,7 @@ const makeFakeProduct = (): any => ({
 describe('AppController', () => {
   let addProductService: AddProductService;
   let productRepository: ProductRepository;
+  let util: Util;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -30,27 +31,40 @@ describe('AppController', () => {
 
     addProductService = app.get<AddProductService>(AddProductService);
     productRepository = app.get<ProductRepository>(ProductRepository);
-  });
-
-  beforeEach(async () => {
-    jest
-      .spyOn(productRepository, 'add')
-      .mockReturnValueOnce(
-        new Promise((resolve) => resolve(makeFakeProduct())),
-      );
+    util = app.get<Util>(Util);
   });
 
   describe('root', () => {
-    it('should addProductRepository with correct values"', async () => {
+    it('should addProductRepository with correct values', async () => {
+      jest
+        .spyOn(productRepository, 'add')
+        .mockReturnValueOnce(
+          new Promise((resolve) => resolve(makeFakeProduct())),
+        );
       const addSpy = jest.spyOn(productRepository, 'add');
       const fakeAddProduct = makeFakeAddProduct();
       await addProductService.add(fakeAddProduct);
       expect(addSpy).toHaveBeenCalledWith(fakeAddProduct);
     });
-    it('should return a product on success"', async () => {
+    it('should return a product on success', async () => {
+      jest
+        .spyOn(productRepository, 'add')
+        .mockReturnValueOnce(
+          new Promise((resolve) => resolve(makeFakeProduct())),
+        );
       const fakeAddProduct = makeFakeAddProduct();
       const product = await addProductService.add(fakeAddProduct);
       expect(product).toEqual(makeFakeProduct());
+    });
+    it('should throw when ProducRepository Throws', async () => {
+      jest.clearAllMocks();
+      const fakeAddProduct = makeFakeAddProduct();
+      jest.spyOn(productRepository, 'add').mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      const promise = addProductService.add(fakeAddProduct);
+      expect(promise).rejects.toThrow();
     });
   });
 });
