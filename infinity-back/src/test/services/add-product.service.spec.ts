1890d5cff35e4d57df536234f8c8b90e48fa87ac
loures90 <fernando.loures@hotmail.com>
2022-08-20 10:03:54 -0300

    test: ensure AAddProductService calls ProductRepository with correct values

---
+++ infinity-back/src/test/services/add-product.service.spec.ts
@@ -0,0 +1,51 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AddProductModel } from '../../domain/usecases/add-product';
+import { AddProductService } from '../../services/add-product.service';
+import { ProductRepository } from '../../repository/product';
+import { Util } from '../../util/util';
+
+const makeFakeAddProduct = (): AddProductModel => ({
+  name: 'any_name',
+  quantity: 'any_quantity',
+  barcode: 'any_barcode',
+  price: '100',
+});
+
+const makeFakeProduct = (): any => ({
+  id: 'any_id',
+  name: 'any_name',
+  quantity: 'any_quantity',
+  barcode: 'any_barcode',
+  price: '100',
+});
+
+describe('AppController', () => {
+  let addProductService: AddProductService;
+  let productRepository: ProductRepository;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [AddProductService, ProductRepository, Util],
+    }).compile();
+
+    addProductService = app.get<AddProductService>(AddProductService);
+    productRepository = app.get<ProductRepository>(ProductRepository);
+  });
+
+  beforeEach(async () => {
+    jest
+      .spyOn(productRepository, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve) => resolve(makeFakeProduct())),
+      );
+  });
+
+  describe('root', () => {
+    it('should addProductRepository with correct values"', async () => {
+      const addSpy = jest.spyOn(productRepository, 'add');
+      const fakeAddProduct = makeFakeAddProduct();
+      await addProductService.add(fakeAddProduct);
+      expect(addSpy).toHaveBeenCalledWith(fakeAddProduct);
+    });
+  });
+});
