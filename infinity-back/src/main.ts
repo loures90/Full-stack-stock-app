501cd502e4828b83fbc39f8c4a5e739da53b85ef
loures90 <fernando.loures@hotmail.com>
2022-08-20 21:39:16 -0300

    chore: enable cors

--- infinity-back/src/main.ts
+++ infinity-back/src/main.ts
@@ -6,6 +6,7 @@ mongoHelper
   .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/products')
   .then(async () => {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     await app.listen(8080);
   })
   .catch(console.error);
