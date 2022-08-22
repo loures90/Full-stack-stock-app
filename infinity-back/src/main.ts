af4d6e83a15ae2a529de8e0a3399dab676a659a4
loures90 <fernando.loures@hotmail.com>
2022-08-22 09:19:17 -0300

    feat: dockerize the application

--- infinity-back/src/main.ts
+++ infinity-back/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import mongoHelper from './repository/mongo-helper';
 
 mongoHelper
-  .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/products')
+  .connect(process.env.MONGO_URL || 'mongodb://mongo:27017/products')
   .then(async () => {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
